0f3c12e782a87d55cbea94e3dbd5bfbc0b268f1b
Jared Hanson <jaredhanson@gmail.com>
2012-07-19 08:17:33 -0700

    Initial commit.

---
+++ bin/rivet.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var program = require('commander')
+  , rivet = require('../')
+
+program.version(rivet.version)
+  .option('-f, --file [file]', 'load tasks from file (default: `rivet.js`)');
+
+program
+  .command('*')
+  .description('execute the given tasks')
+  .action(function(options, task) {
+    // TODO: Figure out a scheme for passing arguments to tasks
+    //console.dir(arguments)
+    //console.dir(arguments[arguments.length - 1].parent)
+    
+    // slice off the last arguement, as it is the `commander.Command` instance
+    // itself
+    var tasks = [].slice.call(arguments, 0, arguments.length - 1);
+    
+    // TODO: exec `default` task, if none specified
+    rivet.cli.exec(this.file || 'rivet.js', tasks);
+  });
+
+program.parse(process.argv);
