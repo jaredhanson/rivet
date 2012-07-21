20f3b361a71459537e30b1e5fab8f3b660b23992
Jared Hanson <jaredhanson@gmail.com>
2012-07-21 13:42:03 -0700

    Switch to optimist for argument parsing.

--- bin/rivet.js
+++ bin/rivet.js
@@ -1,26 +1,19 @@
 #!/usr/bin/env node
 
-var program = require('commander')
-  , rivet = require('../')
+var rivet = require('../')
+  , optimist = require('optimist')
+  , argv = optimist
+    .usage('$0 [-f file] {options} targets')
+    .alias('f', 'file').default('f', 'rivet.js').describe('f', 'file containing tasks to load')
+    .alias('n', 'dry-run').default('n', false).describe('n', 'do a dry run without executing tasks')
+    .alias('q', 'quiet').default('q', false).describe('q', 'do not log messages to standard output')
+    .alias('h', 'help')
+    .argv
 
-program.version(rivet.version)
-  .option('-f, --file [file]', 'load tasks from file (default: `rivet.js`)');
+console.log(argv)
 
-program
-  .command('*')
-  .description('execute the given tasks')
-  .action(function(options, task) {
-    // TODO: Figure out a scheme for passing arguments to tasks
-    //       This may be easier by switching to optimist.
-    //console.dir(arguments)
-    //console.dir(arguments[arguments.length - 1].parent)
-    
-    // slice off the last arguement, as it is the `commander.Command` instance
-    // itself
-    var tasks = [].slice.call(arguments, 0, arguments.length - 1);
-    
-    // TODO: exec `default` task, if none specified
-    rivet.cli.exec(this.file || 'rivet.js', tasks);
-  });
-
-program.parse(process.argv);
+if (argv.help) {
+  optimist.showHelp();
+} else {
+  rivet.cli.exec(argv.file, argv._.length ? argv._ : [ 'default' ], argv);
+}
